05f8b15c08d690c47a1100eec9b16465c773a3c8
bubingy <769004837@qq.com>
2020-11-19 13:41:07 +0800

    config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import os
+import platform
 import configparser
 
 
@@ -36,11 +37,15 @@ class GlobalConfig:
             'win-x64', 'osx-x64', 'linux-x64', 
             'linux-musl-x64', 'linux-arm', 'linux-arm64'
         ]
-        assert isinstance(self.test_bed, os.PathLike)
-
-        if os.path.exists(self.test_bed) is False:
-            os.makedirs(self.test_bed)
+        try:
+            if os.path.exists(self.test_bed) is False:
+                os.makedirs(self.test_bed)
+        except Exception as e:
+            print(e)
+            exit(-1)
+        
         if os.path.exists(self.test_result) is False:
             os.makedirs(self.test_result)
+
 
 configuration = GlobalConfig()
