bd18145cbfc516bd848a97d502ee108b96e91759
bubingy <769004837@qq.com>
2021-04-07 17:37:06 +0800

    dix bugs

--- DiagnosticsTestScripts/test/dotnet_trace.py
+++ DiagnosticsTestScripts/test/dotnet_trace.py
@@ -4,7 +4,7 @@ import os
 import time
 
 from config import configuration
-from utils import run_command_async, run_command_sync
+from utils import run_command_async, run_command_sync, PIPE
 from project import projects
 
 log_path = os.path.join(configuration.test_result, 'dotnet_trace.log')
