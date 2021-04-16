170ccc3f99333ca114d833403c1fca32dea5a325
bubingy <769004837@qq.com>
2021-04-16 14:06:35 +0800

    use new oom project

--- CrossOSDACTestScripts/Projects/oom/Program.cs
+++ CrossOSDACTestScripts/Projects/oom/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
 
 namespace oom
 {
@@ -7,32 +7,9 @@ namespace oom
     {
         public static void Main()
         {
-            Double[] values = GetData();
-            // Compute mean.
-            Console.WriteLine("Sample mean: {0}, N = {1}",
-                                GetMean(values), values.Length);
-        }
-
-        private static Double[] GetData()
-        {
-            Random rnd = new Random();
-            List<Double> values = new List<Double>();
-            for (int ctr = 1; ctr <= 200000000; ctr++) {
-                values.Add(rnd.NextDouble());
-                if (ctr % 10000000 == 0)
-                    Console.WriteLine("Retrieved {0:N0} items of data.",
-                                    ctr);
-            }
-            return values.ToArray();
-        }
-
-        private static Double GetMean(Double[] values)
-        {
-            Double sum = 0;
-            foreach (var value in values)
-                sum += value;
-
-            return sum / values.Length;
+            StringBuilder sb = new StringBuilder(15, 15);
+            sb.Append("Substring #1 ");
+            sb.Insert(0, "Substring #2 ", 1);
         }
     }
 }
