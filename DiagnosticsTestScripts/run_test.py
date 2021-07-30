b4b8ef97da38fb41f048c6780b43aced0031bc44
bubingy <769004837@qq.com>
2021-07-30 13:30:00 +0800

    uncomment main

--- DiagnosticsTestScripts/run_test.py
+++ DiagnosticsTestScripts/run_test.py
@@ -11,18 +11,18 @@ from test import dotnet_trace
 
 if __name__ == '__main__':
     init.prepare_test_bed()
-    # init.install_sdk()
-    # init.install_tools()
+    init.install_sdk()
+    init.install_tools()
 
-    # projects.create_publish_consoleapp()
-    # projects.create_publish_GCDumpPlayground()
-    # projects.create_publish_webapp()
+    projects.create_publish_consoleapp()
+    projects.create_publish_GCDumpPlayground()
+    projects.create_publish_webapp()
 
-    # benchmark.download_diagnostics()
-    # benchmark.run_benchmark()
+    benchmark.download_diagnostics()
+    benchmark.run_benchmark()
 
-    # dotnet_counters.test_counters()
-    # dotnet_dump.test_dump()
-    # dotnet_gcdump.test_gcdump()
-    # dotnet_sos.test_sos()
-    # dotnet_trace.test_trace()
+    dotnet_counters.test_counters()
+    dotnet_dump.test_dump()
+    dotnet_gcdump.test_gcdump()
+    dotnet_sos.test_sos()
+    dotnet_trace.test_trace()
