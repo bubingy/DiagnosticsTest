36552bc04a974e2357e13be580675b62c5e827ab
bubingy <769004837@qq.com>
2021-09-16 12:16:05 +0800

    fix bug of clean.py

--- CrossOSDACTestScripts/clean.py
+++ CrossOSDACTestScripts/clean.py
@@ -9,29 +9,30 @@ from config import GlobalConfig
 def get_remove_candidate(global_conf: GlobalConfig) -> set:
     to_be_removed = set()
     for idx, _ in enumerate(global_conf.sdk_version_list):
-        conf = global_conf.get(idx)
-        if 'win' in conf.rid: home_path = os.environ['USERPROFILE']
-        else: home_path = os.environ['HOME']
-        to_be_removed = to_be_removed.union(
-            {
-                os.path.join(home_path, '.aspnet'),
-                os.path.join(home_path, '.dotnet'),
-                os.path.join(home_path, '.nuget'),
-                os.path.join(home_path, '.templateengine'),
-                os.path.join(home_path, '.lldb'),
-                os.path.join(home_path, '.lldbinit'),
-                os.path.join(home_path, '.local'),
-                conf.dotnet_root,
-                conf.tool_root
-            }
-        )
-        for f in os.listdir(conf.test_bed):
-            if conf.dump_directory == f: continue
-            if conf.analyze_output == f: continue
-
-            to_be_removed.add(
-                os.path.join(conf.test_bed, f)
+        for arch in ['x86', 'x64']:
+            conf = global_conf.get(idx, arch)
+            if 'win' in conf.rid: home_path = os.environ['USERPROFILE']
+            else: home_path = os.environ['HOME']
+            to_be_removed = to_be_removed.union(
+                {
+                    os.path.join(home_path, '.aspnet'),
+                    os.path.join(home_path, '.dotnet'),
+                    os.path.join(home_path, '.nuget'),
+                    os.path.join(home_path, '.templateengine'),
+                    os.path.join(home_path, '.lldb'),
+                    os.path.join(home_path, '.lldbinit'),
+                    os.path.join(home_path, '.local'),
+                    conf.dotnet_root,
+                    conf.tool_root
+                }
             )
+            for f in os.listdir(conf.test_bed):
+                if conf.dump_directory == os.path.join(conf.test_bed, f): continue
+                if conf.analyze_output == os.path.join(conf.test_bed, f): continue
+
+                to_be_removed.add(
+                    os.path.join(conf.test_bed, f)
+                )
     return to_be_removed
 
 
