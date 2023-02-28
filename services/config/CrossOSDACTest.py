bb6f63a789ded96c881d4f10bbd02b2e0d2c20b4
bubingy <769004837@qq.com>
2023-02-28 09:45:52 +0000

    add crossosdac analyze module

---
+++ services/config/CrossOSDACTest.py
@@ -0,0 +1,52 @@
+import os
+import configparser
+
+import instances.config.CrossOSDACTest as CrossOSDACTestConf
+from services.sysinfo import get_rid
+
+
+def load_crossosdactestconf(conf_file_path: os.PathLike=None) -> None:
+    '''Load diagnostic tools test configuration from conf file
+
+    :param conf_file_path: absolute path of conf file
+    :return: None
+    '''
+    config = configparser.ConfigParser()
+
+    config.read(conf_file_path)
+
+    CrossOSDACTestConf.sdk_version_list = config['DotNet']['Version'].split('\n')
+    CrossOSDACTestConf.sdk_version_list.remove('')
+    CrossOSDACTestConf.sdk_buildid_list = config['DotNet']['BuildID'].split('\n')
+    CrossOSDACTestConf.sdk_buildid_list.remove('')
+
+    CrossOSDACTestConf.tool_version = config['DiagTool']['Version']
+    CrossOSDACTestConf.tool_feed = config['DiagTool']['Feed']
+
+    CrossOSDACTestConf.test_name = config['Test']['Name']
+
+    CrossOSDACTestConf.analyze_testbed_root = config['Analyze']['TestBedRoot']
+    CrossOSDACTestConf.use_container = config['Analyze']['Container']
+
+    CrossOSDACTestConf.validate_testbed_root = config['Validate']['TestBedRoot']
+
+    CrossOSDACTestConf.docker_base_url = config['Container']['DockerBaseUrl']
+    CrossOSDACTestConf.dockerfile_url = config['Container']['DockerfileUrl']
+    CrossOSDACTestConf.mount_dir = config['Container']['MountDir']
+    CrossOSDACTestConf.security_opt = config['Container']['SecurityOpt']
+    CrossOSDACTestConf.cap_add = config['Container']['CapAdd']
+    CrossOSDACTestConf.privileged = config['Container']['Privileged']
+
+    CrossOSDACTestConf.analyze_testbed = os.path.join(
+        CrossOSDACTestConf.analyze_testbed_root,
+        f'TestBed-{CrossOSDACTestConf.test_name}'
+    )
+
+    CrossOSDACTestConf.validate_testbed = os.path.join(
+        CrossOSDACTestConf.validate_testbed_root,
+        f'TestBed-{CrossOSDACTestConf.test_name}'
+    )
+
+    CrossOSDACTestConf.rid = get_rid()
+
+    CrossOSDACTestConf.env = os.environ.copy()
--- tasks/CrossOSDACTest/analyze.py
+++ tasks/CrossOSDACTest/analyze.py
@@ -1,12 +1,15 @@
-'''Analyze dumpfiles
-'''
-
-# coding=utf-8
-
 import os
 
-from CrossOSDACTest.config import TestConfig
-from utils.terminal import run_command_async, PIPE
+import instances.constants as constants
+import instances.config.CrossOSDACTest as crossosdac_test_conf
+from instances.logger import ScriptLogger
+from services.terminal import run_command_async, PIPE
+from services.project import oom as oom_service
+from services.project import uhe as uhe_service
+from services.dotnet import sdk as sdk_service
+from services.dotnet import cleaner as cleaner_service
+from services.dotnet import tools as tools_service
+from services.config.CrossOSDACTest import load_crossosdactestconf
 
 
 COMMANDS = [
@@ -23,40 +26,147 @@ COMMANDS = [
 ]
 
 
-def analyze(conf: TestConfig, dump_path: os.PathLike):
-    """ analyze dump file and redirect output to a file
+def prepare_analyze_test_bed():
+    '''Create folders for TestBed.
+    '''
+    try:
+        if not os.path.exists(crossosdac_test_conf.analyze_testbed):
+            os.makedirs(crossosdac_test_conf.analyze_testbed)
+    except Exception as e:
+        print(f'fail to create folders: {e}')
+        exit(-1)
 
-    params
-        dump_path: the path of dump file
-    """
-    dump_name = os.path.basename(dump_path)
 
-    analyze_output_path = os.path.join(
-        conf.analyze_output,
-        dump_name.replace('dump', 'out')
-    )
-    with open(analyze_output_path, 'w+') as stream:
-        if conf.rid == 'linux-musl-arm64':
-            tool_version = conf.tool_version
-            command = (
-                f'{conf.dotnet} '
-                f'{conf.tool_root}/.store/dotnet-dump/{tool_version}/dotnet-dump/{tool_version}/tools/netcoreapp3.1/any/dotnet-dump.dll '
-                f'analyze {dump_path}'
-            )
-        else:
-            dotnet_dump_path = os.path.join(conf.tool_root, 'dotnet-dump')
-            command = f'{dotnet_dump_path} analyze {dump_path}'
-
-        process = run_command_async(
-            command,
-            stdin=PIPE,
-            stdout=stream,
-            stderr=stream
+def analyze():
+    load_crossosdactestconf(
+        os.path.join(
+            constants.configuration_root,
+            'LTTngTest.conf'
         )
-        for analyze_command in COMMANDS:
-            try:
-                process.stdin.write(analyze_command)
-            except Exception as exception:
-                stream.write(f'{exception}\n'.encode('utf-8'))
-                continue
-        process.communicate()
+    )
+    prepare_analyze_test_bed()
+
+    logger = ScriptLogger(
+        'CrossOSDAC-Analyze',
+        os.path.join(crossosdac_test_conf.analyze_testbed, 'CrossOSDAC-Analyze.log')
+    )
+
+    for sdk_version in crossosdac_test_conf.sdk_version_list:
+        env = os.environ.copy()
+        dotnet_root = os.path.join(
+            crossosdac_test_conf.analyze_testbed,
+            f'dotnet-sdk-net{sdk_version}'
+        )
+        tool_root = os.path.join(
+            crossosdac_test_conf.analyze_testbed,
+            f'diag-tool-net{sdk_version}-ver{crossosdac_test_conf.tool_version}'
+        )
+
+        env['DOTNET_ROOT'] = dotnet_root
+        env['PATH'] = f'{dotnet_root}:{tool_root}:' + env['PATH']
+        
+        sdk_service.install_sdk_from_script(
+            sdk_version,
+            crossosdac_test_conf.analyze_testbed,
+            dotnet_root,
+            crossosdac_test_conf.rid,
+            arch=None,
+            logger=logger
+        )
+
+        tools_service.install_tool(
+            os.path.join(dotnet_root, 'dotnet'),
+            'dotnet-dump',
+            tool_root,
+            crossosdac_test_conf.tool_version,
+            crossosdac_test_conf.tool_feed,
+            env,
+            logger
+        )
+
+        dump_path_dir = os.path.join(
+            crossosdac_test_conf.analyze_testbed,
+            f'dumpfiles-net{sdk_version}'
+        )
+
+        analyze_output_dir = os.path.join(
+            crossosdac_test_conf.analyze_testbed,
+            f'analyzeoutput-net{sdk_version}'
+        )
+
+        dotnet_dump_path = os.path.join(tool_root, 'dotnet-dump')
+
+        # create and run oom
+        oom_service.create_build_oom(
+            crossosdac_test_conf.analyze_testbed,
+            os.path.join(dotnet_root, 'dotnet'),
+            env,
+            crossosdac_test_conf.rid,
+            sdk_version,
+            logger
+        )
+        dump_path = oom_service.run_oom(
+            env,
+            dump_path_dir,
+            sdk_version,
+            crossosdac_test_conf.rid,
+            logger
+        )
+        command = f'{dotnet_dump_path} analyze {dump_path}'
+        dump_name = os.path.basename(dump_path)
+        analyze_output_path = os.path.join(
+            analyze_output_dir,
+            dump_name.replace('dump', 'out')
+        )
+        with open(analyze_output_path, 'w+') as stream:
+            process = run_command_async(
+                command,
+                stdin=PIPE,
+                stdout=stream,
+                stderr=stream
+            )
+            for analyze_command in COMMANDS:
+                try:
+                    process.stdin.write(analyze_command)
+                except Exception as exception:
+                    stream.write(f'{exception}\n'.encode('utf-8'))
+                    continue
+            process.communicate()
+            
+        # create and run uhe
+        uhe_service.create_build_uhe(
+            crossosdac_test_conf.analyze_testbed,
+            os.path.join(dotnet_root, 'dotnet'),
+            env,
+            crossosdac_test_conf.rid,
+            sdk_version,
+            logger
+        )
+        dump_path = uhe_service.run_uhe(
+            env,
+            dump_path_dir,
+            sdk_version,
+            crossosdac_test_conf.rid,
+            logger
+        )
+        command = f'{dotnet_dump_path} analyze {dump_path}'
+        dump_name = os.path.basename(dump_path)
+        analyze_output_path = os.path.join(
+            analyze_output_dir,
+            dump_name.replace('dump', 'out')
+        )
+        with open(analyze_output_path, 'w+') as stream:
+            process = run_command_async(
+                command,
+                stdin=PIPE,
+                stdout=stream,
+                stderr=stream
+            )
+            for analyze_command in COMMANDS:
+                try:
+                    process.stdin.write(analyze_command)
+                except Exception as exception:
+                    stream.write(f'{exception}\n'.encode('utf-8'))
+                    continue
+            process.communicate()
+            
